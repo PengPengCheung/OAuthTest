b3e80a79d8e8a9976d4eeb97f2b0460458178fc0
pengpeng <419143166@qq.com>
2015-11-02 14:53:13 +0800

    finish getUserInfo

--- src/com/example/pengpengcheungtest/WBAuthActivity.java
+++ src/com/example/pengpengcheungtest/WBAuthActivity.java
@@ -16,16 +16,13 @@
 
 package com.example.pengpengcheungtest;
 
-import java.text.SimpleDateFormat;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sina.weibo.sdk.auth.AuthInfo;
@@ -33,6 +30,12 @@ import com.sina.weibo.sdk.auth.Oauth2AccessToken;
 import com.sina.weibo.sdk.auth.WeiboAuthListener;
 import com.sina.weibo.sdk.auth.sso.SsoHandler;
 import com.sina.weibo.sdk.exception.WeiboException;
+
+import com.sina.weibo.sdk.net.RequestListener;
+import com.sina.weibo.sdk.openapi.UsersAPI;
+import com.sina.weibo.sdk.openapi.models.ErrorInfo;
+import com.sina.weibo.sdk.openapi.models.User;
+import com.sina.weibo.sdk.utils.LogUtil;
 
 /**
  * 该类主要演示如何进行授权、SSO登陆。
@@ -43,9 +46,6 @@ import com.sina.weibo.sdk.exception.WeiboException;
 public class WBAuthActivity extends Activity {
     
     private static final String TAG = "weibosdk";
-
-    /** 显示认证后的信息，如 AccessToken */
-    private TextView mTokenText;
     
     private AuthInfo mAuthInfo;
     
@@ -54,6 +54,9 @@ public class WBAuthActivity extends Activity {
 
     /** 注意：SsoHandler 仅当 SDK 支持 SSO 时有效 */
     private SsoHandler mSsoHandler;
+    
+    /** 用户信息接口 */
+    private UsersAPI mUsersAPI;
 
     /**
      * @see {@link Activity#onCreate}
@@ -62,86 +65,30 @@ public class WBAuthActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // 获取 Token View，并让提示 View 的内容可滚动（小屏幕可能显示不全）
-//        mTokenText = (TextView) findViewById(R.id.token_text_view);
-//        TextView hintView = (TextView) findViewById(R.id.obtain_token_hint);
-//        hintView.setMovementMethod(new ScrollingMovementMethod());
 
         // 创建微博实例
         //mWeiboAuth = new WeiboAuth(this, Constants.APP_KEY, Constants.REDIRECT_URL, Constants.SCOPE);
         // 快速授权时，请不要传入 SCOPE，否则可能会授权不成功
         mAuthInfo = new AuthInfo(this, Constants.APP_KEY, Constants.REDIRECT_URL, Constants.SCOPE);
         mSsoHandler = new SsoHandler(WBAuthActivity.this, mAuthInfo);
-//        Thread  init =    new  Thread(new Runnable() {
-//            public void run() {
-//                mSsoHandler = new SsoHandler(WBAuthActivity.this, mAuthInfo);
-////                mSsoHandler.authorizeWeb(new AuthListener());
-//                mSsoHandler.authorizeClientSso(new AuthListener());
-//            }
-//        });
-//        
-//        init.start();
-        
-        // SSO 授权, 仅客户端
-//        findViewById(R.id.obtain_token_via_sso).setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                AidInfo  aidInfo   =   AidTask4Plug.getInstance(WBAuthActivity.this, "123456").getAid4PlugSync( "123456", "pak", "hash");
-//                  mSsoHandler.authorizeClientSso(new AuthListener());
-//            }
-//        });
-        
-        // 手机短信授权 
-        //  title 短信注册页面title，可选，不传时默认为""验证码登录""。此处WeiboAuthListener 对象 listener 
-        //可以是和sso 同一个 listener   回调对象 也可以是不同的。开发者根据需要选择
-//        findViewById(R.id.obtain_token_via_mobile).setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                mSsoHandler.registerOrLoginByMobile("验证码登录",new AuthListener());
-//            }
-//        });
-        
-        // SSO 授权, 仅Web
-//        findViewById(R.id.obtain_token_via_web).setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                mSsoHandler.authorizeWeb(new AuthListener());
-//            }
-//        });
         
         // SSO 授权, ALL IN ONE   如果手机安装了微博客户端则使用客户端授权,没有则进行网页授权
         findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mSsoHandler.authorize(new AuthListener());
+                mSsoHandler.authorize(new AuthListener());   
             }
         });
-        
-        // 用户登出
-//        findViewById(R.id.logout).setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                AccessTokenKeeper.clear(getApplicationContext());
-//                mAccessToken = new Oauth2AccessToken();
-//                updateTokenView(false);
-//            }
-//        });
-        
-        // 通过 Code 获取 Token
-//        findViewById(R.id.obtain_token_via_code).setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(WBAuthActivity.this, WBAuthCodeActivity.class));
-//            }
-//        });
 
         // 从 SharedPreferences 中读取上次已保存好 AccessToken 等信息，
         // 第一次启动本应用，AccessToken 不可用
         mAccessToken = AccessTokenKeeper.readAccessToken(this);
         if (mAccessToken.isSessionValid()) {
 //            updateTokenView(true);
+        	mUsersAPI = new UsersAPI(WBAuthActivity.this, Constants.APP_KEY, mAccessToken);
         	Toast.makeText(WBAuthActivity.this, "mAccessToken.isSessionValid", Toast.LENGTH_SHORT).show();
+        	long uid = Long.parseLong(mAccessToken.getUid());
+            mUsersAPI.show(uid, mListener);
         }
     }
 
@@ -175,18 +122,17 @@ public class WBAuthActivity extends Activity {
         public void onComplete(Bundle values) {
             // 从 Bundle 中解析 Token
             mAccessToken = Oauth2AccessToken.parseAccessToken(values);
-            //从这里获取用户输入的 电话号码信息 
-//            String  phoneNum =  mAccessToken.getPhoneNum();
             if (mAccessToken.isSessionValid()) {
-                // 显示 Token
-//                updateTokenView(false);
-                
+            	mUsersAPI = new UsersAPI(WBAuthActivity.this, Constants.APP_KEY, mAccessToken);
                 // 保存 Token 到 SharedPreferences
                 AccessTokenKeeper.writeAccessToken(WBAuthActivity.this, mAccessToken);
+                long uid = Long.parseLong(mAccessToken.getUid());
+                
+                mUsersAPI.show(uid, mListener);
+                Log.i(TAG, "---uid---"+uid);
                 Intent intent = new Intent(WBAuthActivity.this, MainActivity.class);
                 startActivity(intent);
-//                Toast.makeText(WBAuthActivity.this, 
-//                        "success", Toast.LENGTH_SHORT).show();
+                WBAuthActivity.this.finish();
             } else {
                 // 以下几种情况，您会收到 Code：
                 // 1. 当您未在平台上注册的应用程序的包名与签名时；
@@ -215,21 +161,27 @@ public class WBAuthActivity extends Activity {
         }
     }
     
-    /**
-     * 显示当前 Token 信息。
-     * 
-     * @param hasExisted 配置文件中是否已存在 token 信息并且合法
-     */
-//    private void updateTokenView(boolean hasExisted) {
-//        String date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(
-//                new java.util.Date(mAccessToken.getExpiresTime()));
-//        String format = getString(R.string.weibosdk_demo_token_to_string_format_1);
-//        mTokenText.setText(String.format(format, mAccessToken.getToken(), date));
-//        
-//        String message = String.format(format, mAccessToken.getToken(), date);
-//        if (hasExisted) {
-//            message = getString(R.string.weibosdk_demo_token_has_existed) + "\n" + message;
-//        }
-//        mTokenText.setText(message);
-//    }
+    private RequestListener mListener = new RequestListener() {
+        @Override
+        public void onComplete(String response) {
+            if (!TextUtils.isEmpty(response)) {
+                LogUtil.i(TAG, response);
+                // 调用 User#parse 将JSON串解析成User对象
+                User user = User.parse(response);
+                if (user != null) {
+                    Toast.makeText(WBAuthActivity.this, 
+                            "获取User信息成功，用户昵称：" + user.screen_name, 
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(WBAuthActivity.this, response, Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        @Override
+        public void onWeiboException(WeiboException e) {
+            LogUtil.e(TAG, e.getMessage());
+            ErrorInfo info = ErrorInfo.parse(e.getMessage());
+            Toast.makeText(WBAuthActivity.this, info.toString(), Toast.LENGTH_LONG).show();
+        }
+    };
 }
